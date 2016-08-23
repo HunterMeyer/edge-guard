728135f767397332277501cf9d05351bbc6fe800
HunterMeyer <huntermeyer@live.com>
2016-08-22 22:55:11 -0400

    Create countdown clock in JS

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+  // Set Countdown Timer
+  var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+  initializeClock('countdown-div', deadline);
+
   $('.ui.dropdown').dropdown();
 
   $('.ui.sidebar').sidebar('attach events', '.toc.item');
