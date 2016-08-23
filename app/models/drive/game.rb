77e9b40151728d0122539953385de563f38cb73d
HunterMeyer <huntermeyer@live.com>
2016-08-23 00:26:51 -0400

    Add Game list

---
+++ app/models/drive/game.rb
@@ -0,0 +1,5 @@
+class Game < Drive
+  def self.spreadsheet_key
+    '1KG9YzPvqAPt7lT6aTRjJ5tvcbEpnz0a4_rPSGlnG7cQ'
+  end
+end
--- app/models/drive/news.rb
+++ app/models/drive/news.rb
@@ -1,5 +1,5 @@
 class News < Drive
   def self.spreadsheet_key
-    @spreadsheet_key ||= '1Mqi95eAqkCdP_a4VYd9XlLg09fUAaS_20RDi9hfeCWM'
+    '1Mqi95eAqkCdP_a4VYd9XlLg09fUAaS_20RDi9hfeCWM'
   end
 end
