032de41436a51a5b5da26b89cc0fcff4b6e46c40
HunterMeyer <huntermeyer@live.com>
2016-08-25 23:55:03 -0400

    Add Hotel maps image/url, and set countdown

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
-  // Set Countdown Timer
-  var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+  var deadline = new Date('December 10, 2016');
   initializeClock('countdown-div', deadline);
 
   $('.ui.dropdown').dropdown();
 
   $('.ui.sidebar').sidebar('attach events', '.toc.item');
 
-  $('.tabular.menu .item').tab()
-;
+  $('.tabular.menu .item').tab();
 });
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,12 +3,6 @@ module ApplicationHelper
     (page_title.presence || '2016 Gaming Tournament') + ' | Melvin'
   end
 
-  def markdown(text)
-    html = Redcarpet::Render::HTML.new(prettify: true, hard_wrap: true)
-    markdown = Redcarpet::Markdown.new(html, space_after_headers: true, fenced_code_blocks: true)
-    markdown.render(text).html_safe
-  end
-
   def generate_form_errors(errors)
     content_tag(:div, class: 'error') do
       'Oops. Something went wrong:'.html_safe +
@@ -16,5 +10,18 @@ module ApplicationHelper
         errors.full_messages.map { |message| content_tag(:li, message) }.join.html_safe
       end
     end
+  end
+
+  def venue_address
+    '100+N+Atlantic+Ave+Daytona+Beach+FL+32118'
+  end
+
+  def maps_image_url
+    options = { zoom: 11, size: '666x375', markers: venue_address }
+    "https://maps.googleapis.com/maps/api/staticmap?#{options.to_query}"
+  end
+
+  def maps_url
+    "https://www.google.com/maps/place/#{venue_address}"
   end
 end
