84e44f7cf921124cef4aba529c265819c02f56d1
HunterMeyer <huntermeyer@live.com>
2016-08-27 00:32:52 -0400

    Add registration and Subscriber routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Melvin::Application.routes.draw do
-
   root 'home#index'
+  get :register, to: 'home#register'
 
+  resources :players, only: [:new, :create]
+  resources :spectators, only: [:new, :create]
+  resources :volunteers, only: [:new, :create]
 end
