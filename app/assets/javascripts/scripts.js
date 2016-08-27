55998faca7ab2dba9648356d362b193357e7ef02
HunterMeyer <huntermeyer@live.com>
2016-08-27 02:24:36 -0400

    Move countdown scripts off global script page

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-  var deadline = new Date('December 10, 2016');
-  initializeClock('countdown-div', deadline);
-
   $('.ui.dropdown').dropdown();
 
   $('.ui.sidebar').sidebar('attach events', '.toc.item');
