8fee3e6309280fe6d826bdde920dbc9d6ffbb789
HunterMeyer <huntermeyer@live.com>
2016-09-01 23:22:57 -0400

    Add S3 images for Hotel image and Venue map

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,29 +11,6 @@ module ApplicationHelper
     end
   end
 
-  def venue_address
-    '100+N+Atlantic+Ave+Daytona+Beach+FL+32118'
-  end
-
-  def maps_image_url
-    options = { zoom: 9, size: '675x359', markers: venue_address }
-    "https://maps.googleapis.com/maps/api/staticmap?#{options.to_query}"
-  end
-
-  def maps_url
-    "https://www.google.com/maps/place/#{venue_address}"
-  end
-
-  def hotel_url
-    'http://www3.hilton.com/en/hotels/florida/'\
-      'hilton-daytona-beach-resort-ocean-walk-village-DABDHHF/index.html'
-  end
-
-  def hotel_image_url
-    'http://www3.hilton.com/resources/media/hi/DABDHHF/en_US/img/shared/'\
-      'full_page_image_gallery/main/HH_exteriorsky_675x359_FitToBoxSmallDimension_Center.jpg'
-  end
-
   def payment_call_to_action(subscriber)
     discount_end = subscriber.discount_end_date
     return if discount_end.past?
