58e543017949938d283b7fd1361a9410f8208af4
HunterMeyer <huntermeyer@live.com>
2016-09-01 22:47:54 -0400

    Add Faq and Schedule models

---
+++ app/models/drive/faq.rb
@@ -0,0 +1,5 @@
+class Faq < Drive
+  def self.spreadsheet_key
+    '1Qtv0jczwcoRBMO9N0lCRd_k7tx4emzXiijy9xDnNAWc'
+  end
+end
---
+++ app/models/drive/schedule.rb
@@ -0,0 +1,5 @@
+class Schedule < Drive
+  def self.spreadsheet_key
+    '11RxfRgGGotficQ0kaS4YcZUUYYZfQkJyj_eCuBL_3'
+  end
+end
