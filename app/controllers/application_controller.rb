c282f017cadbbf64322f62e5fa76e6f84f15e53c
HunterMeyer <huntermeyer@live.com>
2016-09-07 00:39:28 -0400

    Remove Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   def resource_name
     :user
@@ -20,12 +19,5 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |user, pass|
       user == ENV['MELVIN_USER'] && pass == ENV['MELVIN_PASS']
     end
-  end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.for(:account_update) { |u|
-      u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
-    }
   end
 end
