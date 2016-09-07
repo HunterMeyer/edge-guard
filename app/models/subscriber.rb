0e2e19cb11df0fd4377a2aac67fcb67ccee03b2a
HunterMeyer <huntermeyer@live.com>
2016-09-07 00:33:18 -0400

    Add full_name method to Subscriber

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,4 +1,8 @@
 class Subscriber < ActiveRecord::Base
   validates :first_name, :last_name, :email, presence: true
   attr_accessor :card_token
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
